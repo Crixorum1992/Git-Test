e27ae3ed86b327a26cbd6e12398e8cc18a1672dd
Crixorum1992 <nicjreid@ut.utm.edu>
2017-04-05 21:08:52 -0500

    Commit: updating main project file

--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
 //Name: Nicholas Reid
 //File: main.cpp
-//Description: Hello World
+//Description: Sums
 
 #include <iostream>
 
 using namespace std;
 
+int sum (int n);
+
 int main () {
-  cout<<"Hello World!"<<endl;
+  int n;
+  cout<<"Choose a number"<<endl;
+  cin>>n;
+  cout<<sum(n)<<endl;
+}
+
+int sum(int n){
+  int x=1;
+  int y=0;
+  while (x<=n){
+    y=x+y;
+    x++;
+  }
+  return y;
 }
